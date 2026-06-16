8d8030cce6f2f0824fd1b9a83ab45bf07d2d1dbf
Rahimdalila <leddarahim477@gmail.com>
2026-06-16 23:02:55 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1233,5 +1233,4 @@ if __name__ == "__main__":
     print("  Admin    : http://127.0.0.1:5000/admin")
     print("  URL      : http://127.0.0.1:5000")
     print("=" * 60 + "\n")
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port) 
+    app.run(debug=True, port=5000)
